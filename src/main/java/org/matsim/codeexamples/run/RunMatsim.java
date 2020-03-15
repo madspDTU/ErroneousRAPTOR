3d4fa21f5332f5e0f8d66d82a961a235e723f79e
madspDTU <madsp@dtu.dk>
2020-03-15 20:43:27 +0100

    Setting up the example

--- src/main/java/org/matsim/codeexamples/run/RunMatsim.java
+++ src/main/java/org/matsim/codeexamples/run/RunMatsim.java
@@ -18,12 +18,35 @@
  * *********************************************************************** */
 package org.matsim.codeexamples.run;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.controler.Controler;
-import org.matsim.core.gbl.Gbl;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ModeParams;
+import org.matsim.core.population.routes.GenericRouteImpl;
+import org.matsim.core.router.RoutingModule;
 import org.matsim.core.scenario.ScenarioUtils;
+import ch.sbb.matsim.routing.pt.raptor.DefaultRaptorIntermodalAccessEgress;
+import ch.sbb.matsim.routing.pt.raptor.DefaultRaptorParametersForPerson;
+import ch.sbb.matsim.routing.pt.raptor.DefaultRaptorStopFinder;
+import ch.sbb.matsim.routing.pt.raptor.LeastCostRaptorRouteSelector;
+import ch.sbb.matsim.routing.pt.raptor.RaptorIntermodalAccessEgress;
+import ch.sbb.matsim.routing.pt.raptor.RaptorParametersForPerson;
+import ch.sbb.matsim.routing.pt.raptor.RaptorRouteSelector;
+import ch.sbb.matsim.routing.pt.raptor.RaptorStaticConfig;
+import ch.sbb.matsim.routing.pt.raptor.RaptorStopFinder;
+import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptor;
+import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorData;
+import ch.sbb.matsim.routing.pt.raptor.RaptorStaticConfig.RaptorOptimization;
 
 /**
  * @author nagel
@@ -31,35 +54,149 @@ import org.matsim.core.scenario.ScenarioUtils;
  */
 public class RunMatsim{
 
+
+	static int startTime = 3 * 3600;
+	static int endTime = 27 * 3600;
+
+	private static final double transferBufferTime = 0.;
+
+
+	static final String MODE_TRAIN = "train";
+	static final String MODE_BUS = "bus";
+
+	private static HashSet<String> ptSubModes = new HashSet<String>(Arrays.asList(MODE_TRAIN, MODE_BUS));
+
+	static double waitTimeUtility = -1. * 3600.;
+	static double trainTimeUtility = -1. * 3600.;
+	static double trainDistanceUtility = 0.;
+	static double busTimeUtility = -1. * 3600.;
+	static double busDistanceUtility = 0.;
+	public static double walkTimeUtility = -1. * 3600.;
+	static double walkDistanceUtility = 0.;
+	static final double maxBeelineTransferWalk = 750.; // Furthest walk
+	// between to
+	// _transfer_
+	// stations [m]
+
+	// Never used, except for transfering onto the current map in next timestep.
+
+
+	public static double walkBeelineDistanceFactor = 1.;
+	public static Double walkSpeed = 1.;
+	private static double searchRadius = 3600.; //according to / Andersson 2013 (evt 2016) (60 min walk)
+	private static double extensionRadius = 7200.;
+
+
+
+
 	public static void main(String[] args) {
-		if ( args.length==0 ) {
-			args = new String [] { "scenarios/equil/config.xml" } ;
-			// to make sure that something is run by default; better start from MATSimGUI.
-		} else {
-			Gbl.assertIf( args[0] != null && !args[0].equals( "" ) );
+
+		for(double transferUtility : Arrays.asList(-0., -600.)) {
+			Config config = createConfig(transferUtility);
+
+			RaptorParametersForPerson raptorParams = new DefaultRaptorParametersForPerson(config);
+			RaptorRouteSelector leastCostSelector = new LeastCostRaptorRouteSelector();
+			RaptorIntermodalAccessEgress iae = new DefaultRaptorIntermodalAccessEgress();
+			Map<String, RoutingModule> routingModuleMap = new HashMap<String, RoutingModule>();
+			RaptorStopFinder stopFinder = new DefaultRaptorStopFinder(null, iae, routingModuleMap);
+			RaptorStaticConfig staticConfig = RunMatsim.createRaptorStaticConfig(config);
+
+
+
+			Scenario scenario = ScenarioUtils.loadScenario(config) ;
+			SwissRailRaptorData data = SwissRailRaptorData.create(scenario.getTransitSchedule(), staticConfig ,
+					scenario.getNetwork());
+			SwissRailRaptor raptor = new SwissRailRaptor(data, raptorParams, leastCostSelector, stopFinder);
+
+			for(Person person : scenario.getPopulation().getPersons().values()) {
+				Plan plan = person.getPlans().get(0);
+				Activity firstActivity = (Activity) plan.getPlanElements().get(0);
+				MyFakeFacility firstFacility = new MyFakeFacility(firstActivity.getCoord());
+				Activity lastActivity = (Activity) plan.getPlanElements().get(2);
+				MyFakeFacility lastFacility = new MyFakeFacility(lastActivity.getCoord());
+				List<Leg> path = raptor.calcRoute(firstFacility, lastFacility, firstActivity.getEndTime().seconds(), person);
+
+
+				System.out.println("\n\n When using transfer penalty: " + transferUtility);
+				for(Leg leg : path) {
+					if(leg.getRoute() instanceof GenericRouteImpl) {
+						System.out.println("Walk leg from time " + leg.getDepartureTime() +
+								" to time " + Math.ceil(leg.getDepartureTime() + leg.getTravelTime()) );
+					} else { 
+						System.out.println(leg.getRoute().getRouteDescription());
+					}
+				}
+			}
+
+		}
+	}
+
+
+
+	public static Config createConfig(double transferUtility) {
+		Config config = ConfigUtils.createConfig();
+		config.network().setInputFile("input/network.xml");
+		config.plans().setInputFile("input/PTPlans_CPH_TripBased_1Trip.xml");
+		config.transit().setTransitScheduleFile("input/perfectSchedule_reduced.xml");
+
+
+
+		config.transitRouter().setMaxBeelineWalkConnectionDistance(maxBeelineTransferWalk);
+		config.transitRouter().setSearchRadius(searchRadius); 
+		config.transitRouter().setAdditionalTransferTime(transferBufferTime);
+		config.transitRouter().setExtensionRadius(extensionRadius);
+		config.transitRouter().setDirectWalkFactor(walkBeelineDistanceFactor);
+		config.plansCalcRoute().clearModeRoutingParams();
+
+		for(String mode : Arrays.asList(TransportMode.walk, TransportMode.transit_walk)) {
+			config.plansCalcRoute().getOrCreateModeRoutingParams(mode).setBeelineDistanceFactor(walkBeelineDistanceFactor);
+			config.plansCalcRoute().getOrCreateModeRoutingParams(mode).setTeleportedModeSpeed(walkSpeed);
+			ModeParams walkParams = new ModeParams(mode);
+			walkParams.setMarginalUtilityOfDistance(walkDistanceUtility);
+			walkParams.setMarginalUtilityOfTraveling(walkTimeUtility);
+			config.planCalcScore().addModeParams(walkParams);
 		}
 
-		Config config = ConfigUtils.loadConfig( args ) ;
-		
-		// possibly modify config here
-		
-		// ---
-		
-		Scenario scenario = ScenarioUtils.loadScenario(config) ;
-		
-		// possibly modify scenario here
-		
-		// ---
-		
-		Controler controler = new Controler( scenario ) ;
-		
-		// possibly modify controler here
+		config.planCalcScore().setPerforming_utils_hr(0);
+		config.transit().setTransitModes(ptSubModes);
+		for (String mode : ptSubModes) {
+			ModeParams params = new ModeParams(mode);
+			switch (mode) {
+			case MODE_TRAIN:
+				params.setMarginalUtilityOfTraveling(trainTimeUtility);
+				params.setMarginalUtilityOfDistance(trainDistanceUtility);
+				break;
+			case MODE_BUS:
+				params.setMarginalUtilityOfTraveling(busTimeUtility);
+				params.setMarginalUtilityOfDistance(busDistanceUtility);
+				break;
+			default:
+				break;
+			}
+			config.planCalcScore().addModeParams(params);
+		}
+		config.planCalcScore().setMarginalUtlOfWaitingPt_utils_hr(waitTimeUtility);
+		config.planCalcScore().setUtilityOfLineSwitch(transferUtility);
 
-//		controler.addOverridingModule( new OTFVisLiveModule() ) ;
-		
-		// ---
-		
-		controler.run();
+		config.qsim().setStartTime(RunMatsim.startTime);
+		config.qsim().setEndTime(endTime);
+
+		return config;
 	}
-	
+
+	public static RaptorStaticConfig createRaptorStaticConfig(Config config) {
+		RaptorStaticConfig staticConfig = new RaptorStaticConfig();
+		staticConfig.setBeelineWalkConnectionDistance(config.transitRouter().getMaxBeelineWalkConnectionDistance());
+		staticConfig.setBeelineWalkDistanceFactor(config.transitRouter().getDirectWalkFactor());
+		staticConfig.setBeelineWalkSpeed(config.plansCalcRoute().getOrCreateModeRoutingParams(TransportMode.walk)
+				.getTeleportedModeSpeed());
+		staticConfig.setMinimalTransferTime(config.transitRouter().getAdditionalTransferTime());
+		staticConfig.setOptimization(RaptorOptimization.OneToOneRouting);
+
+		staticConfig.setUseModeMappingForPassengers(true);
+		for (String mode : ptSubModes) {
+			staticConfig.addModeMappingForPassengers(mode, mode);
+		}
+		return staticConfig;
+	}
 }
